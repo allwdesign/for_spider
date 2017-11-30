b89d4bcb79df243b5c4537e46195c39192e29ecd
Natalia Kuzmenko <allwdesign@gmail.com>
2017-11-30 13:42:31 +0300

    Add url for routing OrganizationDetail view

--- core/urls.py
+++ core/urls.py
@@ -5,8 +5,12 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    url(r'^organizations/(?P<district_id>[0-9]+)/$', views.OrganizationList.as_view(),
+    url(r'^organizations/(?P<district_id>[0-9]+)/$',
+        views.OrganizationList.as_view(),
         name='organization-list'),
+    url(r'^organization/(?P<pk>[0-9]+)/$',
+        views.OrganizationDetail.as_view(),
+        name='organization-detail'),
     url(r'^services/(?P<pk>[0-9]+)/$', views.ServiceDetail.as_view(),
         name='service-detail'),
 
