a0943a8c3a1e8254dbe08f3b0f5674af42e7c267
Natalia Kuzmenko <allwdesign@gmail.com>
2017-11-30 11:03:46 +0300

    Add filters by max and min price into OrganizationList view

--- core/views.py
+++ core/views.py
@@ -1,19 +1,35 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, renderers
 from rest_framework.response import Response
 from .models import District, Category, Service, Organization
 from .serializers import OrganizationSerializer, ServiceSerializer
 from core import views
 
+
 class OrganizationList(generics.ListCreateAPIView):
     """Display all Organizations by district"""
     serializer_class = OrganizationSerializer
+    filter_backends = (DjangoFilterBackend,)
 
     def get_queryset(self):
         district = self.kwargs['district_id']
-        return Organization.objects.filter(districts=district)
+        queryset = Organization.objects.filter(districts=district)
 
+        # filtering by max price http://localhost:8000/organizations/5/?max_price=500.0
+        max_price = self.request.query_params.get('max_price', None)
+
+        # filtering by min price http://localhost:8000/organizations/1/?min_price=280.0
+        min_price = self.request.query_params.get('min_price', None)
+
+        if max_price is not None:
+            queryset = queryset.filter(services__price__lte=max_price)
+
+        if min_price is not None:
+            queryset = queryset.filter(services__price__gte=min_price)
+
+        return queryset
 
 
 class ServiceDetail(generics.RetrieveUpdateAPIView):
