ebcee47a1e491230732a5b5ef51e379dc3a0fe52
Natalia Kuzmenko <allwdesign@gmail.com>
2017-12-01 09:29:57 +0300

    Add missing organization detail serializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
-from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import District, Category, Service, Organization
+from core.models import Service, Organization
 
 
 class OrganizationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Organization
+        fields = '__all__'
+
+
+class OrganizationDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Organization
         fields = '__all__'
