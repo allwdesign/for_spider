ed7332ce6b517d1ecf290a6070d9d2efa115a03b
Natalia <31250744+allwdesign@users.noreply.github.com>
2019-11-14 14:24:42 +0300

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -3,17 +3,15 @@
 
 The `urlpatterns` list routes URLs to views.
 """
-from django.conf import settings
-from django.conf.urls import url, include
 from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('core.urls', namespace='core')),
+    path('admin/', admin.site.urls),
+    path('', include('core.urls', namespace='core')),
 ]
 
 # A pattern to include the login and logout views for the browsable API.
 urlpatterns += [
-    url(r'^api-auth/', include('rest_framework.urls',
-                               namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')
 ]
