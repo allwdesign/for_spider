13dc6724b46f6a376791e54c09c0aa896da65662
allwdesign <allwdesign@gmail.com>
2019-12-29 19:45:49 +0300

    Delete related_name parametr from Service model

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,7 +1,15 @@
-# -*- coding: utf-8 -*-
 from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    """Generating Tokens"""
+    if created:
+        Token.objects.create(user=instance)
 
 class District(models.Model):
 	"""District model"""
@@ -30,7 +38,7 @@ class Category(models.Model):
 class Service(models.Model):
 	"""Service model"""
 	service_name = models.CharField(max_length=250)
-	category = models.ForeignKey(Category, related_name='categories', on_delete=models.CASCADE)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
 	price = models.DecimalField(decimal_places=2, max_digits=20)
 
 	class Meta:
@@ -38,7 +46,8 @@ class Service(models.Model):
 		verbose_name_plural = "Services"
 
 	def __str__(self):
-		return self.service_name
+		"""Displays the service and the category to which it belongs"""
+		return f'{self.service_name} - {self.category.category_name}'
 
 
 class Organization(models.Model):
