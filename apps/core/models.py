5aeef566f067c169f2ae8c490ea9eaac877457cb
allwdesign <allwdesign@gmail.com>
2019-12-29 19:52:09 +0300

    Delete User model import

--- apps/core/models.py
+++ apps/core/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
